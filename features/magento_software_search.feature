07ac6fd0f1415c693fc4155092144a650f94aff8
nicofrm <nicoleta_frumosu@ymail.com>
2024-11-19 08:26:44 +0200

    add new files

---
+++ features/magento_software_search.feature
@@ -0,0 +1,9 @@
+Feature: If the user searches for a product, proper results must be returned according
+      to the search criteria and filters
+
+    Scenario: Verify that when the user searches for a keyword, the search returns results according to that keyword
+        Given I am on magento software testing homepage
+        When I click on the search input and I enter "capri" text
+        When I click "ENTER"
+        Then I should have at least "1" results returned
+        Then The search results should have "capri" in the product title
---
+++ pages/search_results_page.py
@@ -0,0 +1,22 @@
+from time import sleep
+
+from selenium.webdriver.common.by import By
+from pages.base_page import Base_Page
+
+class Search_Results_Page(Base_Page):
+    SEARCH_RESULTS = (By.XPATH, '//a[@class="product-item-link"]')
+
+    def check_number_of_search_results(self, number_of_results):
+        search_results = self.driver.find_elements(*self.SEARCH_RESULTS)
+        assert len(search_results) >= int(
+            number_of_results), f'Expected length to be >= {number_of_results}, actual length: {len(search_results)}'
+
+    def check_that_results_are_correct(self, search_value):
+        time.sleep(10)
+        search_results = self.driver.find_elements(*self.SEARCH_RESULTS)
+        is_product_title_correct = True
+        for i in range(len(search_results)):
+            if search_value not in search_results[i].text.lower():
+                is_product_title_correct = False
+                break
+        assert is_product_title_correct == True, "Error: the product title is not correct"
